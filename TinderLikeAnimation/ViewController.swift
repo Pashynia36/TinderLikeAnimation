416ec4c90ac124252f319382573d7b3cf1e65082
Pavlo Novak <>
2018-04-16 10:25:11 +0300

    Added 2 keyFrame animations. (superView & button for shaking subView)

--- TinderLikeAnimation/ViewController.swift
+++ TinderLikeAnimation/ViewController.swift
@@ -11,11 +11,36 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var myView: UIView!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.y"
+        animation.values = [0, 300, 0]
+        animation.keyTimes = [0, 0.5, 1]
+        animation.duration = 3
+        animation.isAdditive = true
+        
+        view.layer.add(animation, forKey: "move")
     }
     
+    func shakeView(view: UIView) {
+        
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, -5, 5, -5, 0]
+        animation.keyTimes = [0, 0.125, 0.250, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
+        animation.duration = 0.4
+        animation.isAdditive = true
+        
+        view.layer.add(animation, forKey: "shake")
+    }
+    
+    @IBAction func shakeIt(_ sender: UIButton) {
+        
+        shakeView(view: myView)
+    }
     @IBAction func handlePan(recognizer: UIPanGestureRecognizer) {
         
         let translation = recognizer.translation(in: self.view)
